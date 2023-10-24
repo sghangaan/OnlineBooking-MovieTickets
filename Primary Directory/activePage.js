a9ddebcb10000868422d7332d95c5a8e63ee5fd4
Sharon Grace T. Hangaan <127215110+SG-Hangaan@users.noreply.github.com>
2023-10-24 20:12:39 +0800

    Delete Primary Directory directory

--- Primary Directory/activePage.js
+++
@@ -1,8 +0,0 @@
-const activePage = window.location.pathname;
-const navLinks = document.querySelectorAll('nav a');
-
-navLinks.forEach(link => {
-    if (link.href.endsWith(activePage)) {
-        link.classList.add('active');
-    }
-});
