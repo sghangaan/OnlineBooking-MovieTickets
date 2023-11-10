1caef7a42084a56027e902da77b4b957f074cbf3
Sharon Grace T. Hangaan <sharonygracehangaan@gmail.com>
2023-11-10 18:58:30 +0800

    update syntax

---
+++ OBMT_Interface/Movie Browsing Selection/BUY Ticket/NSL ch_1-[1.3] NumTicket.js
@@ -0,0 +1,45 @@
+let availableSeat = 36;
+ 
+const minusButton = document.getElementById('minus');
+    const plusButton = document.getElementById('plus');
+    const inputField = document.getElementById('input');
+                        
+    const minValue = 0; // Minimum value
+    const maxValue = 8; // Maximum value
+
+    minusButton.addEventListener('click', event => {
+        event.preventDefault();
+        let currentValue = Number(inputField.value) || 0;
+            if (currentValue > minValue) {
+            inputField.value = currentValue - 1;
+            }
+            });
+
+    plusButton.addEventListener('click', event => {
+        event.preventDefault();
+        let currentValue = Number(inputField.value) || 0;
+            if (currentValue < maxValue) {
+            inputField.value = currentValue + 1;
+            }
+            });
+
+    document.getElementById("input").addEventListener("change", function() {
+        let v = parseInt(this.value);
+            if (v < 1) this.value = 1;
+            if (v > 8) this.value = 8;
+            });
+ 
+function storeName() {
+    // Get the user's name from the input field
+    var b = document.getElementById("input").value;
+    
+
+    // Store the user's name in localStorage
+    localStorage.setItem("b", b);
+}
+
+
+window.onload = function() {
+    let counter = document.getElementById("seats");
+    counter.textContent = `Available Seats: ${availableSeat}`;
+}
